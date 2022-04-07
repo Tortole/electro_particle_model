39f5ae54a9e3a8a5a2af263ea0c3c5d505989374
tortole <xorid@mail.ru>
2022-04-07 20:12:02 +0800

    Added calculation for dipole electric field

--- dipole_magnet_model.py
+++ dipole_magnet_model.py
@@ -5,8 +5,8 @@ from coord import Decart, Polar
 
 
 def calc_B(r: Decart, mu: Decart):
-    return (mu_0 / pi) * (3 * -r * (-r * mu) / r.length**5 - mu / r.length**3)
+    return (mu_0 / pi) * (3 * r * (r * mu) / r.length**5 - mu / r.length**3)
 
 
-def calc_E(r: Decart, mu: Decart):
-    return (1 / 4 * pi * e_0) * (3 * -r * (-r * mu) / r.length**5 - mu / r.length**3)
+def calc_E(r: Decart, p: Decart):
+    return (1 / 4 * pi * e_0) * (3 * -r * (-r * p) / r.length**5 - p / r.length**3)
